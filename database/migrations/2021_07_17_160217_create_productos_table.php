6c83ebea62e7d0e767578440e8ce5213ea906fd6
ArgotePazRamiro <argote_paz_ramiro@hotmail.com>
2021-07-17 19:04:59 -0400

    migraciones

--- database/migrations/2021_07_17_160217_create_productos_table.php
+++ database/migrations/2021_07_17_160217_create_productos_table.php
@@ -14,7 +14,7 @@ class CreateProductosTable extends Migration
     public function up()
     {
         Schema::create('productos', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('nombre');
             $table->string('precioUnitario');
             $table->double('stock', 10, 2);
--- database/migrations/2021_07_17_201103_create_ventas_table.php
+++ database/migrations/2021_07_17_201103_create_ventas_table.php
@@ -14,8 +14,16 @@ class CreateVentasTable extends Migration
     public function up()
     {
         Schema::create('ventas', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->timestamps();
+
+            $table->integer('cliente_id')->unsigned()->nullable();
+            $table->integer('producto_id')->unsigned()->nullable();
+            $table->integer('user_id')->unsigned()->nullable();
+
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('set null');
+            $table->foreign('producto_id')->references('id')->on('productos')->onDelete('set null');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
